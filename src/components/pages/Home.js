e0dd7139d0e8a398666d4cdec0071bf4d4239d2a
nicolasp <84407787+nickxzin@users.noreply.github.com>
2023-04-17 16:04:20 -0300

    Finalizado o landing Page

--- src/components/pages/Home.js
+++
@@ -1,9 +0,0 @@
-import NavBar from "../layout/NavBar"
-
-function Home(){
-    return(
-        <p>123456</p>
-    )
-}
-
-export default Home
--- src/components/scripts/script.js
+++ src/components/scripts/script.js
@@ -1,7 +1,7 @@
-function toggleVideo(){
+function toggleVideo() {
     const trailer = document.querySelector('.trailer');
     const video = document.querySelector('video');
-    trailer.classList.toggle('active')
+    trailer.classList.toggle('active');
 
     video.currentTime = 0;
     video.pause();
